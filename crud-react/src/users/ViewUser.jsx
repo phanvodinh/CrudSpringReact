fb8c8425b5f7f2f04c3488945f1f6eb8077c0127
phanvodinh <19130043@st.hcmuaf.edu.vn>
2023-09-13 17:40:52 +0700

    Update ViewUser.jsx

--- crud-react/src/users/ViewUser.jsx
+++ crud-react/src/users/ViewUser.jsx
@@ -15,6 +15,7 @@ const ViewUser = () => {
     
       useEffect(() => {
         loadUser();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       }, []);
     
       const loadUser = async () => {
